36962f669dadbba7882fb8d0fa366919d3b4a5b2
Konstantin Khaletskiy <konstkhal@gmail.com>
2024-07-26 23:45:04 +0300

    Commit 3

--- main.go
+++ main.go
@@ -4,4 +4,33 @@ import "fmt"
 
 func main() {
   fmt.Println("Hello, People!")
+
+  var insufficientFundMessage string = "Purchase failed. Insufficient funds."
+	var purchaseSuccessMessage string = "Purchase successful."
+	var accountBalance float64 = 100.0
+	var bulkMessageCost float64 = 75.0
+	var isPremiumUser bool = true
+	var discountRate float64 = 0.10
+	var finalCost float64
+
+	// don't edit above this line
+
+	switch isPremiumUser {
+	case true:
+		finalCost = bulkMessageCost * (1 - discountRate)
+
+	default:
+		finalCost = bulkMessageCost
+		}
+	if accountBalance - finalCost > 0 {
+		accountBalance = accountBalance - finalCost
+		fmt.Println(purchaseSuccessMessage)
+	} else {
+		fmt.Println(insufficientFundMessage)
+	}
+	// don't edit below this line
+
+	fmt.Println("Account balance:", accountBalance)
+
+
 }
